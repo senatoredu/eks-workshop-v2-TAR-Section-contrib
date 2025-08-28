4f7567dcb678d17f85228cecf95d84b8209964e4
rivasdam <rivasdam@amazon.com>
2025-08-28 16:57:38 +0000

    Revert "Created new section manifests directory tree to include Kubernetes manifests and Terraform modules. Directory is called traffic-distribution and is located inside networking folder."
    This reverts commit 10f44a18bdf822f817c8284ad04ee2b8d094c730.

--- manifests/modules/networking/traffic-distribution/.workshop/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "environment_variables" {
-  description = "Environment variables to be added to the IDE shell"
-  value = {
-    ADOT_IAM_ROLE = module.iam_assumable_role_adot.iam_role_arn
-  }
-}
