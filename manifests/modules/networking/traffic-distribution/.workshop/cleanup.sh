e6177ec6d69456ae19a8da29cee5acdcb58678d4
rivasdam <rivasdam@amazon.com>
2025-09-22 11:07:59 +0000

    First setup TF script optimization for traffic-distribution section and cleanup script creation. Moved terraform files to .workshop/terraform

---
+++ manifests/modules/networking/traffic-distribution/.workshop/cleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+logmessage "Deleting OpenTelemetry collectors and instrumentation..."
+
+delete-all-if-crd-exists opentelemetrycollectors.opentelemetry.io
+delete-all-if-crd-exists instrumentations.opentelemetry.io
+
+kubectl delete namespace other --ignore-not-found=true
+
+kubectl delete namespace opentelemetry-operator-system --ignore-not-found=true
+
+kubectl delete namespace cert-manager --ignore-not-found=true
+
+uninstall-helm-chart aws-load-balancer-controller kube-system
--- manifests/modules/networking/traffic-distribution/.workshop/terraform/main.tf
+++ manifests/modules/networking/traffic-distribution/.workshop/terraform/main.tf
@@ -1,6 +1,6 @@
 data "aws_partition" "current" {}
 
-module "ebs_csi_driver_irsa" {
+/*module "ebs_csi_driver_irsa" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "5.39.1"
 
@@ -15,8 +15,8 @@ module "ebs_csi_driver_irsa" {
     }
   }
 
-  tags = var.tags
-}
+  tags = var.tags}*/
+
 
 module "eks_blueprints_addons" {
   source  = "aws-ia/eks-blueprints-addons/aws"
@@ -27,14 +27,14 @@ module "eks_blueprints_addons" {
   cluster_version   = var.eks_cluster_version
   oidc_provider_arn = var.addon_context.eks_oidc_provider_arn
 
-  eks_addons = {
+  /*eks_addons = {
     aws-ebs-csi-driver = {
       most_recent              = true
       service_account_role_arn = module.ebs_csi_driver_irsa.iam_role_arn
       preserve                 = false
       configuration_values     = jsonencode({ defaultStorageClass = { enabled = true } })
     }
-  }
+  }*/
 
   enable_aws_load_balancer_controller = true
   aws_load_balancer_controller = {
