19dce902919c0d9e8dce42511b579af392ca9fc1
rivasdam <rivasdam@amazon.com>
2025-09-29 20:06:19 +0000

    Updated cleanup script

--- manifests/modules/networking/traffic-distribution/.workshop/cleanup.sh
+++ manifests/modules/networking/traffic-distribution/.workshop/cleanup.sh
@@ -9,6 +9,13 @@ delete-all-if-crd-exists instrumentations.opentelemetry.io
 
 kubectl delete -n other pod load-generator --ignore-not-found
 
+logmessage "Deleting sequential scaling job and RBAC resources..."
+
+kubectl delete job sequential-scaling -n other --ignore-not-found
+kubectl delete serviceaccount sequential-scaler -n other --ignore-not-found
+kubectl delete clusterrole sequential-scaler --ignore-not-found
+kubectl delete clusterrolebinding sequential-scaler --ignore-not-found
+
 kubectl delete namespace opentelemetry-operator-system --ignore-not-found=true
 
 kubectl delete namespace cert-manager --ignore-not-found=true
